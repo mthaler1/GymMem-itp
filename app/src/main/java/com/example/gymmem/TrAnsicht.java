08771e338a1ce47a2ebb485893ad8b0a9530151c
Marius Thaler <marius.thaler@outlook.com>
2023-04-19 16:13:38 +0200

    Alle Klassen & Layouts (leer)

---
+++ app/src/main/java/com/example/gymmem/TrAnsicht.java
@@ -0,0 +1,14 @@
+package com.example.gymmem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrAnsicht extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tr_ansicht);
+    }
+}
