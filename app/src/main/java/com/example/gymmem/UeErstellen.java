253d4e2036b59e959f510c4608a171e2737ac378
tenzi <tenzi@student.tgm.ac.at>
2023-04-20 14:16:17 +0200

    Die Seite Übung erstellen wurde erstellt. Es gibt eine Texteingabe für den Namen der Übung und ein Dropdown Menü für die Muskelgruppe.

--- app/src/main/java/com/example/gymmem/UeErstellen.java
+++ app/src/main/java/com/example/gymmem/UeErstellen.java
@@ -3,6 +3,8 @@ package com.example.gymmem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class UeErstellen extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class UeErstellen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ue_erstellen);
+
+        Spinner spinnerMuskelgruppen = findViewById(R.id.muskelgruppen);
+        ArrayAdapter<CharSequence>adapter= ArrayAdapter.createFromResource(this, R.array.muskelgruppen, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerMuskelgruppen.setAdapter(adapter);
+
     }
+
+
 }
