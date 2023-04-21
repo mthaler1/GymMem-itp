2477d46727c413c2d0ab61fd71f92fa29c4a7db4
Raphael Tarnoczi <rtarnoczi@student.tgm.ac.at>
2023-04-21 10:18:23 +0200

    Design für TrStarten erstellt

--- app/src/main/java/com/example/gymmem/TrStarten.java
+++ app/src/main/java/com/example/gymmem/TrStarten.java
@@ -3,6 +3,8 @@ package com.example.gymmem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class TrStarten extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class TrStarten extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tr_starten);
+        Spinner spinnerKategorien = findViewById(R.id.auswahlkategorie);
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this, R.array.muskelgruppen, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerKategorien.setAdapter(adapter);
     }
 }
