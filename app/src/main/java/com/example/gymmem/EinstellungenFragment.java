49c5e91b8a829bb2cf3be8bd3c8a76b83dbcaacd
Raphael Tarnoczi <rtarnoczi@student.tgm.ac.at>
2023-04-26 22:10:37 +0200

    Abmeldebutton funktioniert

--- app/src/main/java/com/example/gymmem/EinstellungenFragment.java
+++ app/src/main/java/com/example/gymmem/EinstellungenFragment.java
@@ -1,12 +1,16 @@
 package com.example.gymmem;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class EinstellungenFragment extends Fragment {
 
@@ -16,4 +20,16 @@ public class EinstellungenFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_einstellungen, container, false);
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button logoff = getView().findViewById(R.id.buttonLogOff);
+        logoff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), Login.class));
+            }
+        });
+    }
+
 }
