6c7f6f7c2e3788aee4b3081577646d514fa2dd51
Raphael Tarnoczi <rtarnoczi@student.tgm.ac.at>
2023-04-26 22:31:43 +0200

    Spinner für SatzHinzufuegen gemacht

--- app/src/main/java/com/example/gymmem/SatzHinzufuegen.java
+++ app/src/main/java/com/example/gymmem/SatzHinzufuegen.java
@@ -3,6 +3,8 @@ package com.example.gymmem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SatzHinzufuegen extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class SatzHinzufuegen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_satz_hinzufuegen);
+        Spinner uebung = findViewById(R.id.uebungen);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.uebungen, R.layout.activity_satz_hinzufuegen);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        uebung.setAdapter(adapter);
     }
 }
