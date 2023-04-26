de5b43474f9473f2a0591bb283388bbccd23dbb9
Raphael Tarnoczi <rtarnoczi@student.tgm.ac.at>
2023-04-26 21:47:20 +0200

    Neue Startseite festgelegt, Login und Registrieren funktionsfähig

--- app/src/main/java/com/example/gymmem/Registrieren.java
+++ app/src/main/java/com/example/gymmem/Registrieren.java
@@ -2,7 +2,13 @@ package com.example.gymmem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Registrieren extends AppCompatActivity {
 
@@ -10,5 +16,35 @@ public class Registrieren extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrieren);
+        EditText mail = findViewById(R.id.inputMail);
+        EditText name= findViewById(R.id.inputName);
+        EditText password = findViewById(R.id.inputPassword);
+        Button registrieren = findViewById(R.id.registrierenButton);
+        Button anmelden = findViewById(R.id.loginButton);
+        TextView ausgabe = findViewById(R.id.ausgabe);
+        registrieren.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(TextUtils.isEmpty(name.getText()) || TextUtils.isEmpty(password.getText()) || TextUtils.isEmpty(mail.getText())) {
+                    ausgabe.setText("Mindestens eine Eingabe ist leer!");
+                }
+                else {
+                    if(password.length() > 8) {
+                        ausgabe.setText("Passwort muss mindestens 8 Zeichen lang sein.");
+                    }
+                    else {
+                        startActivity(new Intent(Registrieren.this, Startseite.class));
+                    }
+                }
+
+            }
+        });
+        anmelden.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Registrieren.this, Login.class));
+            }
+        });
+
     }
 }
