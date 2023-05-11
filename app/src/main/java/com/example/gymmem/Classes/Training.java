558720dfe62af366032b05946334b4aa14fee3e6
Marius Thaler <marius.thaler@outlook.com>
2023-05-11 13:22:43 +0200

    ArrayList & Konstruktor hinzugefügt

--- app/src/main/java/com/example/gymmem/Classes/Training.java
+++ app/src/main/java/com/example/gymmem/Classes/Training.java
@@ -1,13 +1,23 @@
 package com.example.gymmem.Classes;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 public class Training {
     private String name;
     private Date dateStart, dateEnd;
+    private List<TrainingSet> sets;
     TrainingType type;
     UUID id;
+
+    public Training(String name, TrainingType type, Date dateStart) {
+        this.setName(name);
+        this.setType(type);
+        this.setDateStart(dateStart);
+        this.sets = new ArrayList<>();
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -21,4 +31,13 @@ public class Training {
         this.dateEnd = dateEnd;
     }
 
+    public void setType(TrainingType type) {
+        this.type = type;
+    }
+
+    public void addSet(TrainingSet set) {
+        if(set != null) {
+            this.sets.add(set);
+        }
+    }
 }
