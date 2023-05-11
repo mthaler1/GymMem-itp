251edf6212f9103b3b59d20459e55b7b24f3470f
Marius Thaler <marius.thaler@outlook.com>
2023-05-11 12:48:08 +0200

    Attribute implementiert

--- app/src/main/java/com/example/gymmem/Classes/Training.java
+++ app/src/main/java/com/example/gymmem/Classes/Training.java
@@ -1,4 +1,11 @@
 package com.example.gymmem.Classes;
 
+import java.util.Date;
+import java.util.UUID;
+
 public class Training {
+    private String name;
+    private Date dateStart, dateEnd;
+    TrainingType type;
+    UUID id;
 }
