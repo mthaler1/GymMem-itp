5eedebc34e8c2536bcde705709cdef7740395182
Raphael Tarnoczi <rtarnoczi@student.tgm.ac.at>
2023-05-17 22:51:34 +0200

    Fragment Help created

---
+++ app/src/main/java/com/example/gymmem/HelpFragment.java
@@ -0,0 +1,23 @@
+package com.example.gymmem;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.View;
+
+public class HelpFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
