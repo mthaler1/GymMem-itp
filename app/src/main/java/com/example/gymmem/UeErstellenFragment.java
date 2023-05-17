2cdb468dd826cf79a69817fcc1d5322a862036db
Marius Thaler <marius.thaler@outlook.com>
2023-05-17 16:49:24 +0200

    Exercise wird in der Datenbank gespeichert

--- app/src/main/java/com/example/gymmem/UeErstellenFragment.java
+++ app/src/main/java/com/example/gymmem/UeErstellenFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.example.gymmem.Classes.Exercise;
+import com.example.gymmem.Classes.ExerciseType;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UeErstellenFragment extends Fragment {
 
@@ -43,6 +56,31 @@ public class UeErstellenFragment extends Fragment {
                     ausgabe.setText("Lege einen Name für die Übung fest!");
                 }
                 else {
+                    Log.i("Eingabe", input.getText().toString());
+                    String exerciseName = input.getText().toString();
+                    Exercise exercise = new Exercise(exerciseName, ExerciseType.ABS);
+                    Map<String, Object> exercises = new HashMap<>();
+                    exercises.put("name",exerciseName);
+                    DocumentReference docRef = FirebaseFirestore.getInstance().collection("Exercise").document(exercise.getName());
+
+                    docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            DocumentSnapshot document = task.getResult();
+                            if(document.exists()) {
+                                ausgabe.setText("Eine Übung unter diesem Namen existiert bereits.");
+                            } else {
+                                docRef.set(exercises);
+                                Log.i("Exercise angelegt",exercise.getName());
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            new IllegalArgumentException();
+                        }
+                    });
+
                     ausgabe.setText(null);
                     input.setText(null);
                 }
